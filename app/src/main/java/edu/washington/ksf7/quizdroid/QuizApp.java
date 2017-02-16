53be07e84351c2ed85192156cf8a3d41125d1306
Keegan Farley <keegomyneego@live.com>
2017-02-15 16:35:15 -0800

    made QuizApp a singleton with access to TopicRepository instance

--- app/src/main/java/edu/washington/ksf7/quizdroid/QuizApp.java
+++ app/src/main/java/edu/washington/ksf7/quizdroid/QuizApp.java
@@ -3,18 +3,35 @@ package edu.washington.ksf7.quizdroid;
 import android.app.Application;
 import android.util.Log;
 
+import edu.washington.ksf7.quizdroid.Repositories.TopicRepository;
+
 /**
  * Created by keegomyneego on 2/14/17.
  */
 
 public class QuizApp extends Application {
 
-    public static final String TAG = "QuizApp";
+    private static final String TAG = "QuizApp";
+
+    private static QuizApp instance;
+
+    private TopicRepository topicRepository;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         Log.d(TAG, "QuizApp created");
+
+        QuizApp.instance = this;
+        this.topicRepository = TopicRepository.getInstance();
+    }
+
+    public QuizApp getInstance() {
+        return instance;
+    }
+
+    public TopicRepository getTopicRepository() {
+        return topicRepository;
     }
 }
